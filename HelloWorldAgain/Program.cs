5176e41ce02c79b906d6eba0ba0e0dded093f5dc
Amy Lee <amy.english.developer@gmail.com>
2021-06-21 08:42:38 +0100

    Moving tutorial exercises into GIT

---
+++ HelloWorldAgain/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge < 0 || userAge > 100)
+            {
+                Console.WriteLine("Invalid Age");
+                Console.WriteLine("Age must be between 0 and 100");
+            }
+            else if (userAge < 18)
+                Console.WriteLine("Sorry you are underage");
+            else if (userAge < 21)
+                Console.WriteLine("You need parental consent");
+            else
+            {
+                Console.WriteLine("Congratulations!");
+                Console.WriteLine("You may sign up for the event!");
+            }
+            Console.Write("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello World! My name is {0} and I am {1} years old. I was born in {2}.", userName, userAge, currentYear - userAge);
+            Console.Read();
+        }
+    }
+}
